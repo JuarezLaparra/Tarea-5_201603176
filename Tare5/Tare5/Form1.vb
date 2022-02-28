34821b92946b969dd21a5762c7a15bbe238a754c
JaniaJuarez <janiajuarez@gmail.com>
2022-02-27 19:47:55 -0600

    TAREA5

---
+++ Tare5/Tare5/Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If MsgBox("Desea salir de la aplicación", vbQuestion + vbYesNo, "Pregunta") = vbYes Then
+            End
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim fac As Long = 1
+        Dim i As Long
+        Dim a As Integer = Int32.Parse(TextBox1.Text)
+        Dim resul As String = ""
+        Dim res As Integer = 0
+
+        While (a >= 0)
+            If (a Mod 4 = 0) Then
+
+                If IsNumeric(TextBox1.Text) Then
+                    For i = CDbl(TextBox1.Text) To 1 Step -1
+                        fac *= i
+                    Next i
+                    TextBox2.Text = fac.ToString
+                End If
+                TextBox3.Text = TextBox3.Text + a.ToString + ","
+                TextBox3.Text = TextBox3.Text + 1
+
+            End If
+        End While
+
+
+
+    End Sub
+End Class
